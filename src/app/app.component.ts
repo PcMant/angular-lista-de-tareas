11d45e1fc2de1d21625dbc1001737d8c3a6b8c3a
PcMant <juanmgpt@gmail.com>
2021-06-09 09:57:15 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,56 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Propiedadea//
+  /** Array que almacena las tareas */
   elementosLista: string[] = [];
+  /** Póxima tarea a guardar */
   newElement: string;
-  contador: number = 0;
+  /** Contenido para el texto de boton destinado a la visibilidad de tareas */
+  BotonVisibilidadTareas: string = 'Ocultar elementos';
+  /** Testigo para poder determinar si ocultar o no la lista de tareas */
+  testigoVisibilidadTareas: boolean = false;
+  estadoItem = 'line-throught';
 
+  //Métodos//
+  /**
+   * Método que añade tareas a la lista
+   */
   add(): void {
     this.elementosLista.push(this.newElement);
     this.newElement = '';
   }
 
+  /**
+   * Método que elimina una tarea en concreto según su indice
+   */
   borrarElementoLista(indice: number): void {
     this.elementosLista.splice(indice, 1);
+  }
+
+  /**
+   * Método que elimina todas las tareas
+   */
+  vaciarElementosLista(): void {
+    this.elementosLista.splice(0);
+  }
+
+  /**
+   * Método para determinar si mostrar o ocultar la lista de tareas
+   */
+  visibilidadTareas(): void {
+    if (
+      this.BotonVisibilidadTareas == 'Ocultar elementos' &&
+      !this.testigoVisibilidadTareas
+    ) {
+      this.BotonVisibilidadTareas = 'Mostrar elementos';
+      this.testigoVisibilidadTareas = true;
+    } else if (
+      this.BotonVisibilidadTareas == 'Mostrar elementos' &&
+      this.testigoVisibilidadTareas
+    ) {
+      this.BotonVisibilidadTareas = 'Ocultar elementos';
+      this.testigoVisibilidadTareas = false;
+    }
   }
 }
